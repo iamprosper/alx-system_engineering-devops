0f8cea0bc9174ee08a25624876370887e90a6f33
iamprosper <thinkpadprosper@gmail.com>
2022-09-18 19:53:34 +0000

    Install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # This file install flask from pip3
-exec {'install flask from pip3',
-  command => 'pip3 install flask==2.1.0',
-  path    => ['/usr/bin']
+package {'flask':
+  ensure      => '2.1.0',
+  provider    => 'pip3'
 }
