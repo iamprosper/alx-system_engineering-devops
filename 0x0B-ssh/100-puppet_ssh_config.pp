885d65946f2a8217e75ff49073e6fe2b4fb55a55
iamprosper <thinkpadprosper@gmail.com>
2022-09-18 21:29:13 +0000

    Modiying ssh configuration file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# This file contain puppet script for ssh conf
+exec { 'Change ssh configuration':
+  command => 'echo IdentityFile ~/.ssh/school\nPasswordAuthentication no >> /etc/ssh/ssh_config',
+  path    => '/usr/bin'
+}
