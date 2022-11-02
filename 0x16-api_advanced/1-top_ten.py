cc23abb5dd425148146246f4a5d08a868c871786
iamprosper <thinkpadprosper@gmail.com>
2022-11-02 13:49:29 +0000

    Checking correct posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,7 +15,7 @@ def top_ten(subreddit):
     """
     header = {'User-agent': 'Chrome'}
     query = {'limit': '10'}
-    response = requests.get("https://reddit.com/r/{}/new.json"
+    response = requests.get("https://reddit.com/r/{}/hot.json"
                             .format(subreddit),
                             headers=header,
                             params=query)
