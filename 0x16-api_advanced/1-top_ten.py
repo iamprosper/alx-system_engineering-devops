34d2cc370e836ac39efa5b13d19c460f8fd4857c
iamprosper <thinkpadprosper@gmail.com>
2022-11-02 01:11:59 +0000

    Task 1 - Fetching the top posts on a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module contain a function that fetch the top ten subreddit
+Function: top_ten(subreddit)
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """
+    This function fetches the top ten subreddit
+    Args:
+        -subreddit
+    """
+    header = {'User-agent': 'Chrome'}
+    query = {'limit': '10'}
+    response = requests.get("https://reddit.com/r/{}/new.json"
+                            .format(subreddit),
+                            headers=header,
+                            params=query)
+    posts = response.json()["data"]["children"]
+    for post in posts:
+        print(post["data"]["title"])
